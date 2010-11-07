8fb2b4e00d5cb3a278661b0141a35e0cccf377cf
Shabda Raaj <shabda@agiliq.com>
2010-11-07 12:30:47 +0530

    Webpy appp

---
+++ webpy/main.py
@@ -0,0 +1,28 @@
+import web
+
+from werkzeug.contrib.cache import SimpleCache
+cache = SimpleCache()
+
+import urllib
+import json as simplejson
+
+urls = (
+  '/', 'index'
+)
+
+class index:
+    def GET(self):
+            fml_endpoint = 'http://graph.facebook.com/search?q="so%20starving&type=post'
+            fb_data = cache.get(key = fml_endpoint)
+            if not fb_data:
+                fb_response = urllib.urlopen(fml_endpoint,).read()
+                fb_data = simplejson.loads(fb_response)["data"]
+                cache.set(fml_endpoint, fb_data, 30 * 60)
+            index = web.template.frender('templates/index.html')
+            return index(fb_data)
+
+
+app = web.application(urls, globals())
+
+if __name__ == "__main__":
+    app.run()
