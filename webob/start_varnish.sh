ab39efea356aa78b13acc96d7a2124a0e3fc370b
Eric Moritz <eric@themoritzfamily.com>
2010-11-08 02:06:47 +0000

    Changed the port so that all the tests could be ran in a batch

--- webob/start_varnish.sh
+++ webob/start_varnish.sh
@@ -1,3 +1,3 @@
 # Start varnish with a size of 1M, we are after all just caching a 1 12K HTML
 # file.
-sudo varnishd -F -f ./varnish.vcl -s malloc,1M -T 127.0.0.1:2000 -a 127.0.0.1:10001
+sudo varnishd -F -f ./varnish.vcl -s malloc,1M -T 127.0.0.1:2000 -a 127.0.0.1:10002
