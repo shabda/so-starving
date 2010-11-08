5d2dbde2060bd01b9cbf79e73c23c895c1b417bb
Didip Kerabat <didip@didip-osx.local>
2010-11-07 20:11:16 -0800

    Updated tornado example to use AsyncHTTPClient

--- tornado/app.py
+++ tornado/app.py
@@ -1,3 +1,4 @@
+import tornado.httpclient
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
@@ -5,20 +6,23 @@ import tornado.web
 from werkzeug.contrib.cache import SimpleCache
 cache = SimpleCache()
 
-import simplejson
 import urllib
 
 class MainHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
     def get(self):
-        #self.write("Hello, world")
-        #return
-        fml_endpoint = 'http://graph.facebook.com/search?q="so%20starving&type=post'
-        fb_data = cache.get(key = fml_endpoint)
+        self.fml_endpoint = 'http://graph.facebook.com/search?q="so%20starving&type=post'
+        fb_data = cache.get(key = self.fml_endpoint)
         if not fb_data:
-            fb_response = urllib.urlopen(fml_endpoint,).read()
-            fb_data = simplejson.loads(fb_response)["data"]
-            cache.set(fml_endpoint, fb_data, 30 * 60)
+            http = tornado.httpclient.AsyncHTTPClient()
+            http.fetch(self.fml_endpoint, callback=self.on_response)
+
+    def on_response(self, response):
+        if response.error: raise tornado.web.HTTPError(500)
+        fb_data = tornado.escape.json_decode(response.body)["data"]
+        cache.set(self.fml_endpoint, fb_data, 30 * 60)
         return self.render('templates/index.html', data=fb_data)
+
 
 application = tornado.web.Application([
     (r"/", MainHandler),
